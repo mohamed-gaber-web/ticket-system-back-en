27e933f7449fd41ca6d86b9c108a6796c7334376
mohamed gaber <mgaberweb@gmail.com>
2025-12-21 06:01:58 +0200

    chore: update server port to 8000 and bind to all network interfaces

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ const startServer = async () => {
     await connectDB();
 
     // Then start the Express server
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server is running on port ${PORT}`);
       console.log(
         `API Documentation available at: http://localhost:${PORT}/api-docs`
