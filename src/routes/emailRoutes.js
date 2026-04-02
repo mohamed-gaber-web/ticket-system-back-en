eeddfb58caa5eda696cdc2bccc1e828886946d60
mohamed gaber <mgaberweb@gmail.com>
2026-04-02 22:18:07 +0200

    chore: enhance config-check to show all MS_ env keys found

--- src/routes/emailRoutes.js
+++ src/routes/emailRoutes.js
@@ -22,12 +22,15 @@ router.get("/test", sendTestEmail);
 
 // GET /api/emails/config-check - Check which env vars are set (values hidden)
 router.get("/config-check", (req, res) => {
+  const allKeys = Object.keys(process.env).filter(k => k.startsWith("MS_"));
   res.json({
     MS_TENANT_ID: !!process.env.MS_TENANT_ID,
     MS_CLIENT_ID: !!process.env.MS_CLIENT_ID,
     MS_CLIENT_SECRET: !!process.env.MS_CLIENT_SECRET,
     MS_EMAIL_FROM: process.env.MS_EMAIL_FROM || null,
     NODE_VERSION: process.version,
+    all_MS_keys_found: allKeys,
+    total_env_vars: Object.keys(process.env).length,
   });
 });
 
